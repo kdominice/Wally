af1bb6e8cd6121e68a8d1f089c82b9cf100e3bbb
Mike Dominice <mike@mikedominice.com>
2012-06-27 23:32:50 -0400

    refactored module exports to clean up the code, refactored bin script to update its dependencies

--- bot.js
+++ bot.js
@@ -1,62 +1,37 @@
+var _ = require('underscore');
 var winston = require('winston');
-
 var Bot = require('ttapi');
+var fs = require('fs');
+var async = require('async');
+
+var templates = require('./lib/templates').templates;
 var TTAdapter = require('./lib/ttadapter');
 var logging = require('./lib/logging').logging;
 var ControllerBot = require('./lib/controller').ControllerBot;
 var commands = require('./lib/commands').commands;
-
 var echonest = require('./lib/echonest').echonest;
 
 var mixins = (function () {
-    
-    var roomLogger = new require('./lib/mixins/roomlogger').RoomLogger({
-            logger : logging.fileLogger('room.log')
+
+    var mixinClasses = ['roomlogger', 'fillindj', 'playtracker', 'bopper', 'greeter', 'snagger',
+        'echonestsessionmanager', 'chatcommandexecutor'];
+        
+    return _.map(mixinClasses, function (mixinClass) {
+        var MixinClass = require('./lib/mixins/' + mixinClass);
+        return new MixinClass({
+            logger : logging.fileLogger(mixinClass + '.log')
         });
-    
-    var fillInDj = new require('./lib/mixins/fillindj').FillInDJ({
-            logger : logging.fileLogger('fillindj.log')
-        });
-    
-    var playTracker = new require('./lib/mixins/playtracker').PlayTracker({
-            logger : logging.fileLogger('playtracker.log')
-        });
-    
-    var bopper = new require('./lib/mixins/bopper').Bopper({
-            logger : logging.fileLogger('bopper.log')
-        });
-    
-    var greeter = new require('./lib/mixins/greeter').Greeter({
-            logger : logging.fileLogger('greeter.log')
-        });
-    
-    var snagger = new require('./lib/mixins/snagger').Snagger({
-            logger : logging.fileLogger('snagger.log')
-        });
-    
-    var echonestSessionManager = new require('./lib/mixins/echonestsessionmanager').EchonestSessionManager({
-            logger : logging.fileLogger('snagger.log')
-        });
-    
-    var chatCommandExecutor = new require('./lib/mixins/chatcommandexecutor').ChatCommandExecutor({
-            logger : logging.fileLogger('snagger.log')
-        });
-    
-    return [roomLogger, fillInDj, playTracker, bopper, snagger, echonestSessionManager,
-        chatCommandExecutor, greeter];
+    });
     
 }).call(this);
-
-var fs = require('fs');
-var async = require('async');
-var templates = require('./lib/templates').templates;
 
 var files = ['./conf.json', './aliases.json', './templateText.json'];
 
 async.map(files, fs.readFile, function (error, results) {
-    var CONFIG = JSON.parse(results[0].toString('ascii'));
-    var aliases = JSON.parse(results[1].toString('ascii'));
-    var templateDefinitions = JSON.parse(results[2].toString('ascii'));
+    var parsedResults = _.chain(results).invoke('toString', 'ascii').map(JSON.parse).value();
+    var CONFIG = parsedResults[0];
+    var aliases = parsedResults[1];
+    var templateDefinitions = parsedResults[2];
     
     commands.loadAliases(aliases);
     templates.load(templateDefinitions);
